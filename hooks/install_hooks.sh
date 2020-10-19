56b2b3271c3e7abc5f91ee1051d6a983dae76f5b
mysterico <ian@mystericoui-iMac.local>
2020-10-19 21:45:54 +0900

    chore: commit-msg 테스트

--- hooks/install_hooks.sh
+++ hooks/install_hooks.sh
@@ -6,9 +6,12 @@ echo "$ROOT_DIR"
 
 ln -s $ROOT_DIR/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
 ln -s $ROOT_DIR/hooks/pre-push .git/hooks/pre-push
+ln -s $ROOT_DIR/hooks/commit-msg .git/hooks/commit-msg
 
 chmod +x .git/hooks/prepare-commit-msg
 chmod +x .git/hooks/pre-push
+chmod +x .git/hooks/commit-msg
 
 echo "Success to inject prepare-commit-msg in git hooks"
 echo "Success to inject pre-push in git hooks"
+echo "Success to inject commit-msg in git hooks"
--- test.sh
+++ test.sh
@@ -1,10 +1,37 @@
 # echo $(git log --pretty=format:"%s")
 
+# LATEST_TAG=$(git describe --tags --abbrev=0)
+# MAJOR=0
+# MINOR=0
+# PATCH=0
 
-git log --pretty=format:"%s" | while read line
+# echo $LATEST_TAG
+
+# git log --pretty=format:"%s" | while read line
+# do
+#   if [[ $line =~ '^fix:' ]];
+#   then
+#     PATCH+=1
+#     echo "0.0.1"
+#   elif [[ $line =~ '^feat:' ]];
+#   then
+#     MINOR+=1
+#     echo "0.1.0"
+#   elif [[ $line =~ '^BREAKING CHANGE:' ]];
+#   then
+#     MAJOR+=1
+#     echo "1.0.0"
+#   else
+#     echo "$line"
+#   fi
+# done
+
+
+# echo "$MAJOR.$MINOR.$PATCH"
+
+
+
+git log --pretty=format:"%H - %aI : %s" | while read line
 do
-  echo $line
+  echo "$line"
 done
-
-
-echo $(git describe --tags)
