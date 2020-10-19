f53491551c85e563ee8f4dd20be92a4b5cb61860
mysterico <ian@mystericoui-iMac.local>
2020-10-19 18:28:13 +0900

    feat: pre-push 적용

--- hooks/install_hooks.sh
+++ hooks/install_hooks.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(pwd)"
 echo "$ROOT_DIR"
 
 ln -s $ROOT_DIR/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
+ln -s $ROOT_DIR/hooks/pre-push .git/hooks/pre-push
 
 chmod +x .git/hooks/prepare-commit-msg
 chmod +x .git/hooks/pre-push
