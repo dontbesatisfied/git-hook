9bba9a81f6e850edc4284333bb5245f1ee5bbe86
mysterico <ian@mystericoui-iMac.local>
2020-10-19 23:29:48 +0900

    feat: log2

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
-LATEST_TAG=$(git describe --tags --abbrev=0)
-LATEST_TAG_TIME=$(echo $(git show -q v0.0.3 --pretty=format:%ai) | grep -Eo '\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\+\d{4}')
+# LATEST_TAG=$(git describe --tags --abbrev=0)
+# LATEST_TAG_TIME=$(echo $(git show -q v0.0.3 --pretty=format:%ai) | grep -Eo '\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\+\d{4}')
 
 
-git log --pretty=format:"%H - %aI : %s" --after="($LATEST_TAG_TIME)" | while read line
-do
-  echo "$line"
-done >> changelog.txt
+# git log --pretty=format:"%H - %aI : %s" --after="($LATEST_TAG_TIME)" | while read line
+# do
+#   echo "$line"
+# done >> changelog.txt
 
 
 
+echo $(date -u +"%Y-%m-%dT%H:%M:%SZ")
