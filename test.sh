49f70908b5617672291453170ca4910136da3a6f
mysterico <ian@mystericoui-iMac.local>
2020-10-19 23:01:13 +0900

    chore: 마지막 태그기준으로 커밋 로그 가져오기

--- test.sh
+++ test.sh
@@ -1,41 +1,8 @@
-# echo $(git log --pretty=format:"%s")
-
 LATEST_TAG=$(git describe --tags --abbrev=0)
-LATEST_TAG_TIME=$(git log -1 --format=%ai $LLATEST_TAG)
-# MAJOR=0
-# MINOR=0
-# PATCH=0
-
-# echo $LATEST_TAG
-
-# git log --pretty=format:"%s" | while read line
-# do
-#   if [[ $line =~ '^fix:' ]];
-#   then
-#     PATCH+=1
-#     echo "0.0.1"
-#   elif [[ $line =~ '^feat:' ]];
-#   then
-#     MINOR+=1
-#     echo "0.1.0"
-#   elif [[ $line =~ '^BREAKING CHANGE:' ]];
-#   then
-#     MAJOR+=1
-#     echo "1.0.0"
-#   else
-#     echo "$line"
-#   fi
-# done
+LATEST_TAG_TIME=$(echo $(git show -q v0.0.3 --pretty=format:%ai) | grep -Eo '\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\+\d{4}')
 
 
-# echo "$MAJOR.$MINOR.$PATCH"
-
-
-
-git log --pretty=format:"%H - %aI : %s" --after="$LATEST_TAG_TIME" | while read line
+git log --pretty=format:"%H - %aI : %s" --after="($LATEST_TAG_TIME)" | while read line
 do
   echo "$line"
 done
-
-echo $(git log -1 --format=%ai $LLATEST_TAG)
-
