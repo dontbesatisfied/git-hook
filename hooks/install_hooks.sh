6e34a2c1e027769a1f38979548ddbce10e7f503f
mysterico <ian@mystericoui-iMac.local>
2020-10-19 18:21:59 +0900

    chore: pre-push 테스트

--- hooks/install_hooks.sh
+++ hooks/install_hooks.sh
@@ -7,5 +7,6 @@ echo "$ROOT_DIR"
 ln -s $ROOT_DIR/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
 
 chmod +x .git/hooks/prepare-commit-msg
+chmod +x .git/hooks/pre-push
 
-echo "commitLint 적용 성공."
+echo "commitLint 적용 성공"
